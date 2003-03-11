0dfb17584972128abc8c6db2971ca11600c5ad9a
nigel <nigel>
2003-03-11 11:31:56 +0000

    32bit defaults for windowed mode.

--- src/MacOSX/PrefsEditor.mm
+++ src/MacOSX/PrefsEditor.mm
@@ -2,7 +2,7 @@
  *	PrefsEditor.m - GUI stuff for Basilisk II preferences
  *					(which is a text file in the user's home directory)
  *
- *	$Id: PrefsEditor.mm,v 1.5 2002/12/18 11:56:12 nigel Exp $
+ *	$Id: PrefsEditor.mm,v 1.6 2003/03/11 11:31:56 nigel Exp $
  *
  *  Basilisk II (C) 1997-2001 Christian Bauer
  *
@@ -326,7 +326,12 @@
 		}
 
 		if ( display_type == DISPLAY_SCREEN )	// If changing from full screen
+        {
 			newx = MIN_WIDTH, newy = MIN_HEIGHT;
+#ifndef MAC_OS_X_VERSION_SUPPORTS_LOWER_DEPTHS
+            newbpp = 32;
+#endif
+        }
 
 		[width	setIntValue: newx];
 		[height setIntValue: newy];
@@ -334,6 +339,15 @@
 	}
 	else
 	{
+#ifndef MAC_OS_X_VERSION_SUPPORTS_LOWER_DEPTHS
+		// Check depth
+
+		if ( display_type == DISPLAY_WINDOW && newbpp != 32 )
+		{
+			WarningSheet(@"Sorry - In windowed mode, depth must be 32", panel);
+			[depth setIntValue: 32];
+		}
+#endif		
 		// Check size is within ranges of MIN_WIDTH ... MAX_WIDTH
 		//							and MIN_HEIGHT ... MAX_HEIGHT
 		// ???
